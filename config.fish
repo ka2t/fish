da5d69cd557116a8cf282af1c7104eaa2f36a904
Kani Takanori <ka2@noroi.local>
2019-11-24 18:41:53 +0900

    initial commit

---
+++ config.fish
@@ -0,0 +1,22 @@
+set -x LANG ja_JP.UTF-8
+export ALTERNATE_EDITOR=""
+set -x EDITOR "emacsclient -t"
+set -x VISUAL "emacsclient -t"
+set -x PATH ~/bin ~/go/bin ~/.local/bin $PATH
+
+# directoryをを省略表示しない
+set -g fish_prompt_pwd_dir_length 0
+
+# alias
+alias e="emacsclient -t"
+alias ekill="emacsclient -e '(kill-emacs)'"
+
+# Key binding
+function fish_user_key_bindings
+        bind \eh backward-kill-word
+        bind \eg peco_select_ghq_repository
+        bind \cx\cg peco_open_gh_repository
+        bind \cr peco_select_history
+        bind \cx\ck peco_kill
+        bind \ec peco_recentd
+end
