10cdcb0e2f6ebade79b7bfeba7a59e5ff8b7ab2d
Kani Takanori <ka2@noroi.local>
2019-12-23 00:48:43 +0900

    24bit color

--- config.fish
+++ config.fish
@@ -3,6 +3,7 @@ export ALTERNATE_EDITOR=""
 set -x EDITOR "emacsclient -t"
 set -x VISUAL "emacsclient -t"
 set -x PATH ~/bin ~/go/bin ~/.local/bin $PATH
+export TERM=xterm-24bit
 
 # directoryをを省略表示しない
 set -g fish_prompt_pwd_dir_length 0
@@ -15,7 +16,6 @@ alias ekill="emacsclient -e '(kill-emacs)'"
 function fish_user_key_bindings
         bind \eh backward-kill-word
         bind \eg peco_select_ghq_repository
-        bind \cx\cg peco_open_gh_repository
         bind \cr peco_select_history
         bind \cx\ck peco_kill
         bind \ec peco_recentd
