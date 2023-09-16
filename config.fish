8ca52befb9e9426490913ee11e7b1f0fb44c64b3
ka2 <ka2@xxx.com>
2023-09-17 00:32:28 +0900

    Add vscode setting

--- config.fish
+++ config.fish
@@ -1,16 +1,24 @@
 set -x LANG ja_JP.UTF-8
-export ALTERNATE_EDITOR=""
-set -x EDITOR "emacsclient -t"
-set -x VISUAL "emacsclient -t"
-set -x PATH ~/bin ~/go/bin ~/.local/bin $PATH
 export TERM=xterm-24bit
 
-# directoryをを省略表示しない
+# PATH
+fish_add_path /usr/local/bin
+fish_add_path $HOME/bin
+fish_add_path $HOME/go/bin
+fish_add_path $HOME/.ghcup/bin
+
+# vscode
+string match -q "$TERM_PROGRAM" "vscode"
+and . (code --locate-shell-integration-path fish)
+
+
+# directoryを省略表示しない
 set -g fish_prompt_pwd_dir_length 0
 
+# rbenv
+#status --is-interactive; and source (rbenv init -|psub)
+
 # alias
-alias e="emacsclient -t"
-alias ekill="emacsclient -e '(kill-emacs)'"
 
 # Key binding
 function fish_user_key_bindings
@@ -21,7 +29,12 @@ function fish_user_key_bindings
         bind \ec peco_recentd
 end
 
+# pyenv
+#set PATH $HOME/.pyenv/shims $HOME/.nodebrew/current/bin $PATH
+#eval (pyenv init - | source)
+
 # [fisher]
+# oh-my-fish/plugin-pbcopy
 # oh-my-fish/plugin-peco
 # oh-my-fish/theme-fox
 # tsu-nera/fish-peco_recentd
